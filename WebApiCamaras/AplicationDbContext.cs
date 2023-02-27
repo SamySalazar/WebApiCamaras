5a7c73ab3f5e28f13155e8560bde6b34dfdf0e1d
SamySalazar <samuel.salazarll@uanl.edu.mx>
2023-02-26 18:18:30 -0600

    CamarasController y DbContext

---
+++ WebApiCamaras/AplicationDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebApiCamaras.Entidades;
+
+namespace WebApiCamaras
+{
+    public class AplicationDbContext : DbContext
+    {
+        public AplicationDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Camara> Camaras { get; set; }
+    }
+}
--- WebApiCamaras/Program.cs
+++ WebApiCamaras/Program.cs
@@ -1,12 +1,12 @@
+using WebApiCamaras;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-
-
+var startup = new Startup(builder.Configuration);
+startup.ConfigureServices(builder.Services);
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-
+startup.Configure(app, app.Environment);
 
 app.Run();
